b46aaf1bf2ee7e9ee47cdbdad6633bfd90e561d6
Byuxavi <u175484601@byui.edu>
2025-05-09 08:49:27 +0200

    OK

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,13 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {   
-        // Ask the user for their name.
-        Console.WriteLine("What is your first name? ");
-        string first = Console.ReadLine(); 
+    {
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last = Console.ReadLine(); 
+        string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
